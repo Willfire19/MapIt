4b7342c3c99932c97e90020b21f8fefe0c0b4c7c
Willfire19 <willfire19@gmail.com>
2013-11-12 00:46:53 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapIt::Application.config.secret_token = '1d831d4656f16e331b14b9c76117f94d610d0f6f2a56a830324988f0788fd0fd07bfbc7af0685d30430bc42811e8d3de435839e3cac1fe3cd3c626a422ad6525'
