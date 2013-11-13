8b0f43ea5cd2e8a6f528304f6799ab169dbd303c
Willfire19 <willfire19@gmail.com>
2013-11-13 14:30:39 -0500

    Added Users using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 MapIt::Application.routes.draw do
+
+  devise_for :users
+
+  root :to => 'home#index'
+
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
